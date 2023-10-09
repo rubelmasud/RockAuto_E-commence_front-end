36473af71d165b6a23163afacb652543710c1a37
Your Name <you@example.com>
2023-10-09 09:47:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Wel come to my Rock Auto Shop</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
