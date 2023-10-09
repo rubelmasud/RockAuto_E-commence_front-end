5a85f55392ec6348bd4cc604285da245c89f9dc2
Your Name <you@example.com>
2023-10-09 17:48:06 +0600

    add footer

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,5 +1,104 @@
+import "./Footer.css";
+import {
+  BiLogoFacebook,
+  BiLogoTwitter,
+  BiLogoYoutube,
+  BiPhoneCall,
+} from "react-icons/bi";
+import { TfiEmail } from "react-icons/tfi";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { FaCcDiscover, FaCcPaypal } from "react-icons/fa";
+import { AiFillAmazonSquare } from "react-icons/ai";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className=" md:flex ">
+        <div className="links-section w-[75%] grid lg:grid-cols-3 md:grid-cols-2 justify-between items-center">
+          <div className="">
+            <h3>OUR COMPANY</h3>
+            <div className="footer-links">
+              <a href="">My Account</a>
+              <a href="">Specials</a>
+              <a href="">Brands</a>
+              <a href="">Order History</a>
+              <a href="">Contact Us</a>
+            </div>
+          </div>
+          <div className="">
+            <h3>EXTRAS</h3>
+            <div className="footer-links">
+              <a href="">Brands</a>
+              <a href="">Affiliate</a>
+              <a href="">Gift Certificates</a>
+              <a href="">Specials</a>
+              <a href="">Site Map</a>
+            </div>
+          </div>
+          <div className="">
+            <h3>MY ACCOUNT</h3>
+            <div className="footer-links">
+              <a href="">My Account</a>
+              <a href="">Specials</a>
+              <a href="">Brands</a>
+              <a href="">Order History</a>
+              <a href="">Contact Us</a>
+            </div>
+          </div>
+        </div>
+        <div className="contact-info w-[25%] text-[#f7c906] mt-3">
+          <div className="flex flex-col gap-4">
+            <div className="flex gap-2">
+              <BiPhoneCall size={28} />
+              <p className="text-[#d1d5dbba] text-[14px]">+123 456 778</p>
+            </div>
+            <div className="flex gap-2">
+              <TfiEmail size={24} />
+              <p className="text-[#d1d5dbba] text-[14px]">
+                {" "}
+                demoexample@gmail.com
+              </p>
+            </div>
+            <div className="flex gap-2">
+              <HiOutlineLocationMarker size={28} />
+              <p className="text-[#d1d5dbba] text-[14px]">
+                151, Demo Store United States
+              </p>
+            </div>
+          </div>
+          <div className="text-white my-4 flex gap-4">
+            <div className="">
+              <BiLogoFacebook
+                size={28}
+                className="bg-blue-900 p-2 rounded-full hover:bg-red-500 duration-300"
+              />
+            </div>
+            <div className="">
+              <BiLogoTwitter
+                size={28}
+                className="bg-blue-600 rounded-full p-2 hover:bg-red-500 duration-300"
+              />
+            </div>
+            <div className="">
+              <BiLogoYoutube
+                size={28}
+                className="bg-red-600 rounded-full p-2 hover:bg-red-500 duration-300"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-[0.5px] bg-slate-500 my-10"></div>
+      <div className="flex justify-between">
+        <h5 className="text-[17px]">Powered By OpenCart Your Store © 2023</h5>
+        <div className="flex gap-4">
+          <FaCcPaypal size={28} />
+          <FaCcDiscover className="text-blue-300" size={28} />
+          <AiFillAmazonSquare className="text-red-300" size={28} />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import routes from "./routes/Routers";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="lg:w-[1240px] mx-auto px-8">
+    <div className="lg:w-[1280px] mx-auto px-8">
       <RouterProvider router={routes} />
     </div>
   </React.StrictMode>
